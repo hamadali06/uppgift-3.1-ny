d69a41c1bdbcbbade8b6c4ee0d389a17301e8863
hamadali06 <ahal06003@utb.vaxjo.se>
2023-09-29 10:57:04 +0200

    Add project files.

---
+++ uppgift 3.1 ny/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur gammal är du? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 16 && age <= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen!");
+            }
+            else
+            {
+                Console.WriteLine("Du får inte delta i tävlingen.");
+            }
+        }
+    }
+}
